b1c7c0ea51641e9a4f56c0e03c0028957e0df61a
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2024-04-20 12:31:08 +0200

    close and open functional for tools button

--- old/src/components/ToolsButton.jsx
+++ old/src/components/ToolsButton.jsx
@@ -1,18 +1,25 @@
-import {Fragment, useState} from "react";
+import {Fragment, useRef, useState} from "react";
 
 export default function ToolsButton({colorPicker, changeColor}) {
-    const [isOpen, setIsOpen] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
+    const container = useRef();
+
+    // Use optional chaining to access container.current safely
+    if (container.current) {
+        container.current.style.display = isOpen ? "block" : "none";
+    }
+
     return (
         <Fragment>
             <button
-                className={`mapStyleButton ${
-                    isOpen ? "mapStyleButton_open" : ""
+                className={`BrusselsButton BrusselsButton_bg ${
+                    isOpen ? "BrusselsButton_open" : ""
                 }`}
                 onClick={() => setIsOpen(!isOpen)}
             >
                 tools
             </button>
-            <div id='toolsContainer'>
+            <div id='toolsContainer' ref={container}>
                 <input
                     type="color"
                     ref={colorPicker}
